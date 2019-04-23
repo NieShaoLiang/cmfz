87faca678ab8f15386811b3138b4facd5133e545
NieShaoLiang <49525809+NieShaoLiang@users.noreply.github.com>
2019-04-23 09:15:28 +0800

    Update CmfzNslApplicationTests.java

--- src/test/java/com/baizhi/CmfzNslApplicationTests.java
+++ src/test/java/com/baizhi/CmfzNslApplicationTests.java
@@ -49,7 +49,7 @@ public class CmfzNslApplicationTests {
     public void t2() {
         Map map = userService.selectActiveNumber();
         System.out.println(map);
-        /hahahahaha
+        //hahahahaha
     }
 
 }
